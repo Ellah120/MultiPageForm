e650018fdc841d577c74e9e1a68bc5e73f75d898
Ellah120 <nwokeellah88@gmail.com>
2023-01-08 18:20:12 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+
 import Home from './home';
 function App() {
   return (
--- src/Components/Sidebar/index.js
+++ src/Components/Sidebar/index.js
@@ -1,23 +1,23 @@
 import bgDesktop from './bgDesktop.svg';
-
+import './sidebar.css';
 function Sidebar() {
   return (
     <div className="sidebar" style={{backgroundImage: `url(${bgDesktop})`, backgroundSize: '250px' , backgroundRepeat: 'no-repeat', width: '500px', height: '800px'}}>
     <div className='one'>
       <p className='numOne'>1</p>
-      <p className='stepOne'>STEP 1 <br /><span>YOUR INFO</span></p>
+      <p className='stepOne'> <span>STEP 1 </span><br />YOUR INFO</p>
     </div>
     <div className='two'>
       <p className='numTwo'>2</p>
-      <p className='stepTwo'>STEP 2 <br /><span>SELECT PLAN</span></p>
+      <p className='stepTwo'><span>STEP 2 </span><br />SELECT PLAN</p>
     </div>
     <div className='three'>
       <p className='numThree'>3</p>
-      <p className='stepThree'>STEP 3 <br /><span>ADD-ONS</span></p>
+      <p className='stepThree'><span>STEP 3 </span><br />ADD-ONS</p>
     </div>
     <div className='four'>
       <p className='numFour'>4</p>
-      <p className='stepFour'>STEP 4 <br /><span>SUMMARY</span></p>
+      <p className='stepFour'><span>STEP 4 </span><br />SUMMARY</p>
     </div>
     </div>
   )
